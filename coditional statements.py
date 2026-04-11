17b310e5a07252c135e71aa565250519a23060b0
codewithsana-sss <vincenzoilrenero@gmail.com>
2026-04-11 15:15:10 +0530

    Add files via upload

---
+++ coditional statements.py
@@ -0,0 +1,11 @@
+num=4
+if num>0:
+    print(num,"the number is positive")
+
+    num=-2
+    if num<0:
+        print(num,"the number is negative")
+
+        num=0
+        if num==0:
+            print(num,"the number is zero")
