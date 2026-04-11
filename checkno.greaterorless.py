b6f922a872d1a68233e54366b6c7139dfc6f91a2
codewithsana-sss <vincenzoilrenero@gmail.com>
2026-04-11 15:35:37 +0530

    Add files via upload

---
+++ checkno.greaterorless.py
@@ -0,0 +1,9 @@
+i=int(input("Enter a number: "))
+if(i<15):
+    print("The i is less than 15.")
+    print("i 'm in if block")
+
+else:
+    print("The i is greater than or equal to 15.")
+    print("i 'm in else block")
+    print("i'm is not in if and not in else block")
